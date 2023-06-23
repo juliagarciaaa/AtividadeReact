1a000cf3845e5b8f69c5d1c215813b45b6f71707
juliagarciaaa <juliasilvagarcia3@gmail.com>
2023-06-23 10:41:41 -0300

    comentário

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-Import React, {useState} from ´react´;
-function App() {
-   const [projects, setProjects] = useState([])
-    function handleAddProject(){
-      setProjects([...projects, `Novo projeto ${Date.now()}`]);
-    }
+export function App(){
   return (
-    <>
-    <header>Conteúdo da Home</header>
-    <main>
-      <ul>
-        {projects.map(item => <List key=(item</List>))}
-      </ul>
-      <button onClick={handleAddProject}>Adicionar</button>
-    </main>
-    </>
-    
+    <h1>Olá</h1>
   )
 }
 
-export default App
